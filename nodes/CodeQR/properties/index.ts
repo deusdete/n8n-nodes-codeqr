eb27203e5fff029ba35abca7e1ddcbb13a9b9896
Deusdete Ferreira <deuzinh2010@gmail.com>
2025-04-30 22:56:35 -0300

    feat(CodeQR): Add QR Code fields and operations for create, update, and delete functionalities; update version to 0.2.4

--- nodes/CodeQR/properties/index.ts
+++ nodes/CodeQR/properties/index.ts
@@ -1,5 +1,7 @@
 import { INodeProperties } from 'n8n-workflow';
+import { linkFields } from './link.fields';
 import { linkOperationsOptions } from './link.operations';
+import { qrcodeFields } from './qrcode.fields';
 import { qrcodeOperationsOptions } from './qrcode.operations';
 const resourcesOptions: INodeProperties = {
 	displayName: 'Resource',
@@ -23,4 +25,6 @@ export const codeqrNodeProperties = [
 	resourcesOptions,
 	linkOperationsOptions,
 	qrcodeOperationsOptions,
+	...linkFields,
+	...qrcodeFields,
 ];
--- nodes/CodeQR/properties/qrcode.fields.ts
+++ nodes/CodeQR/properties/qrcode.fields.ts
@@ -1,6 +1,26 @@
 import { INodeProperties } from 'n8n-workflow';
 
 export const qrcodeFields: INodeProperties[] = [
+	/* -------------------------------------------------------------------------- */
+	/*                                 qrcode:get                                   */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'QR Code ID',
+		name: 'qrcodeId',
+		type: 'string',
+		default: '',
+		required: true,
+		description: 'The ID of the QR Code to retrieve',
+		displayOptions: {
+			show: {
+				resource: ['qrcode'],
+				operation: ['get'],
+			},
+		},
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 qrcode:create                                */
+	/* -------------------------------------------------------------------------- */
 	{
 		displayName: 'QR Code Type',
 		name: 'type',
@@ -21,116 +41,292 @@ export const qrcodeFields: INodeProperties[] = [
 		default: 'url',
 		required: true,
 		description: 'The type of QR Code to generate',
+		displayOptions: {
+			show: {
+				resource: ['qrcode'],
+				operation: ['create'],
+			},
+		},
 	},
 	{
 		displayName: 'Static/Dynamic',
 		name: 'static',
 		type: 'boolean',
 		default: true,
-		required: true,
 		description: 'Whether the QR Code is static (fixed content) or dynamic (editable content)',
-	},
-	{
-		displayName: 'Text',
-		name: 'text',
-		type: 'string',
-		default: '',
-		description: 'Text content stored in the QR Code',
 		displayOptions: {
 			show: {
-				type: ['text'],
+				resource: ['qrcode'],
+				operation: ['create'],
 			},
 		},
 	},
 	{
-		displayName: 'URL',
-		name: 'url',
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ['qrcode'],
+				operation: ['create'],
+			},
+		},
+		options: [
+			{
+				displayName: 'Background Color',
+				name: 'bgColor',
+				type: 'color',
+				default: '',
+				description: 'Background color of the QR Code',
+			},
+			{
+				displayName: 'Description',
+				name: 'description',
+				type: 'string',
+				default: '',
+				description: 'Description associated with the QR Code',
+			},
+			{
+				displayName: 'Email',
+				name: 'email',
+				type: 'json',
+				default: '',
+				description: 'Email data for email-based QR Codes',
+			},
+			{
+				displayName: 'Expiration Date',
+				name: 'expiresAt',
+				type: 'dateTime',
+				default: '',
+				description: 'Expiration date of the QR Code',
+			},
+			{
+				displayName: 'Foreground Color',
+				name: 'fgColor',
+				type: 'color',
+				default: '',
+				description: 'Foreground color of the QR Code',
+			},
+			{
+				displayName: 'Image URL',
+				name: 'image',
+				type: 'string',
+				default: '',
+				description: 'URL of an image associated with the QR Code',
+			},
+			{
+				displayName: 'Phone',
+				name: 'phone',
+				type: 'string',
+				default: '',
+				description: 'Phone number stored in the QR Code',
+			},
+			{
+				displayName: 'Public Stats',
+				name: 'publicStats',
+				type: 'boolean',
+				default: false,
+				description: 'Whether the QR Code statistics are publicly accessible',
+			},
+			{
+				displayName: 'Show Logo',
+				name: 'showLogo',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to display a logo in the QR Code',
+			},
+			{
+				displayName: 'Size',
+				name: 'size',
+				type: 'number',
+				default: 300,
+				description: 'Size of the QR Code in pixels',
+			},
+			{
+				displayName: 'Text',
+				name: 'text',
+				type: 'string',
+				default: '',
+				description: 'Text content stored in the QR Code',
+			},
+			{
+				displayName: 'Title',
+				name: 'title',
+				type: 'string',
+				default: '',
+				description: 'Title associated with the QR Code',
+			},
+			{
+				displayName: 'Track Conversion',
+				name: 'trackConversion',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to enable tracking of conversions for the QR Code',
+			},
+			{
+				displayName: 'URL',
+				name: 'url',
+				type: 'string',
+				default: '',
+				description: 'The destination URL of the QR Code',
+			},
+		],
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 qrcode:update                                */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'QR Code ID',
+		name: 'id',
 		type: 'string',
 		default: '',
-		description: 'The destination URL of the QR Code',
+		required: true,
+		description: 'The ID of the QR Code to update',
+		displayOptions: {
+			show: {
+				resource: ['qrcode'],
+				operation: ['update'],
+			},
+		},
 	},
 	{
-		displayName: 'Email',
-		name: 'email',
-		type: 'json',
-		default: '',
-		description: 'Email data for email-based QR Codes',
+		displayName: 'Update Fields',
+		name: 'updateFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ['link'],
+				operation: ['update'],
+			},
+		},
+		options: [
+			{
+				displayName: 'Background Color',
+				name: 'bgColor',
+				type: 'color',
+				default: '',
+				description: 'Background color of the QR Code',
+			},
+			{
+				displayName: 'Description',
+				name: 'description',
+				type: 'string',
+				default: '',
+				description: 'Description associated with the QR Code',
+			},
+			{
+				displayName: 'Email',
+				name: 'email',
+				type: 'json',
+				default: '',
+				description: 'Email data for email-based QR Codes',
+			},
+			{
+				displayName: 'Expiration Date',
+				name: 'expiresAt',
+				type: 'dateTime',
+				default: '',
+				description: 'Expiration date of the QR Code',
+			},
+			{
+				displayName: 'Foreground Color',
+				name: 'fgColor',
+				type: 'color',
+				default: '',
+				description: 'Foreground color of the QR Code',
+			},
+			{
+				displayName: 'Image URL',
+				name: 'image',
+				type: 'string',
+				default: '',
+				description: 'URL of an image associated with the QR Code',
+			},
+			{
+				displayName: 'Phone',
+				name: 'phone',
+				type: 'string',
+				default: '',
+				description: 'Phone number stored in the QR Code',
+			},
+			{
+				displayName: 'Public Stats',
+				name: 'publicStats',
+				type: 'boolean',
+				default: false,
+				description: 'Whether the QR Code statistics are publicly accessible',
+			},
+			{
+				displayName: 'Show Logo',
+				name: 'showLogo',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to display a logo in the QR Code',
+			},
+			{
+				displayName: 'Size',
+				name: 'size',
+				type: 'number',
+				default: 300,
+				description: 'Size of the QR Code in pixels',
+			},
+			{
+				displayName: 'Static/Dynamic',
+				name: 'static',
+				type: 'boolean',
+				default: true,
+				description: 'Whether the QR Code is static (fixed content) or dynamic (editable content)',
+			},
+			{
+				displayName: 'Text',
+				name: 'text',
+				type: 'string',
+				default: '',
+				description: 'Text content stored in the QR Code',
+			},
+			{
+				displayName: 'Title',
+				name: 'title',
+				type: 'string',
+				default: '',
+				description: 'Title associated with the QR Code',
+			},
+			{
+				displayName: 'Track Conversion',
+				name: 'trackConversion',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to enable tracking of conversions for the QR Code',
+			},
+			{
+				displayName: 'URL',
+				name: 'url',
+				type: 'string',
+				default: '',
+				description: 'The destination URL of the QR Code',
+			},
+		],
 	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 qrcode:delete                                */
+	/* -------------------------------------------------------------------------- */
+
 	{
-		displayName: 'Phone',
-		name: 'phone',
+		displayName: 'QR Code ID',
+		name: 'qrcodeId',
 		type: 'string',
 		default: '',
-		description: 'Phone number stored in the QR Code',
-	},
-	{
-		displayName: 'Expiration Date',
-		name: 'expiresAt',
-		type: 'dateTime',
-		default: '',
-		description: 'Expiration date of the QR Code',
-	},
-	{
-		displayName: 'Track Conversion',
-		name: 'trackConversion',
-		type: 'boolean',
-		default: false,
-		description: 'Whether to enable tracking of conversions for the QR Code',
-	},
-	{
-		displayName: 'Title',
-		name: 'title',
-		type: 'string',
-		default: '',
-		description: 'Title associated with the QR Code',
-	},
-	{
-		displayName: 'Description',
-		name: 'description',
-		type: 'string',
-		default: '',
-		description: 'Description associated with the QR Code',
-	},
-	{
-		displayName: 'Image URL',
-		name: 'image',
-		type: 'string',
-		default: '',
-		description: 'URL of an image associated with the QR Code',
-	},
-	{
-		displayName: 'Background Color',
-		name: 'bgColor',
-		type: 'color',
-		default: '',
-		description: 'Background color of the QR Code',
-	},
-	{
-		displayName: 'Foreground Color',
-		name: 'fgColor',
-		type: 'color',
-		default: '',
-		description: 'Foreground color of the QR Code',
-	},
-	{
-		displayName: 'Size',
-		name: 'size',
-		type: 'number',
-		default: 300,
-		description: 'Size of the QR Code in pixels',
-	},
-	{
-		displayName: 'Show Logo',
-		name: 'showLogo',
-		type: 'boolean',
-		default: false,
-		description: 'Whether to display a logo in the QR Code',
-	},
-	{
-		displayName: 'Public Stats',
-		name: 'publicStats',
-		type: 'boolean',
-		default: false,
-		description: 'Whether the QR Code statistics are publicly accessible',
+		required: true,
+		description: 'The ID of the QR Code to delete',
+		displayOptions: {
+			show: {
+				resource: ['qrcode'],
+				operation: ['delete'],
+			},
+		},
 	},
 ];
